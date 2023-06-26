e97d5814d652ad5c6d28aa0a6dfcd7846d6f4bac
Jamie Bursey <jamie4551@gmail.com>
2023-06-26 19:37:15 -0230

    add row button added

--- Priority.js
+++ Priority.js
@@ -1,4 +1,4 @@
-const tableHeader = ["Name", "Treatment", "Price", "Claimed"];
+const tableHeader = ["Name", "Treatment", "Numer", "Price", "Claimed"];
 const tableElement = document.getElementById("tableElement");
 
 const headerRow = document.createElement("tr");
@@ -9,14 +9,26 @@ tableHeader.forEach((item) => {
     header.classList.add("table-header")
     header.innerHTML = item;
     headerRow.appendChild(header);
-    const tableData = document.createElement("td")
-    tableData.classList.add("table-data")
-    const dataInput = document.createElement("input")
-    dataInput.type = "text"
-    dataInput.classList.add("data-input")
-    tableData.appendChild(dataInput)
-    tableElement.appendChild(tableData)
 
 });
 
+const addRow = () => {
+    const tableRow = document.createElement("tr")
 
+    tableHeader.forEach(() => {
+        const tableData = document.createElement("td")
+        tableData.classList.add("table-data")
+        const dataInput = document.createElement("input")
+        dataInput.type = "text"
+        dataInput.classList.add("data-input")
+        tableData.appendChild(dataInput)
+        tableRow.appendChild(tableData)
+    })
+    tableElement.appendChild(tableRow)
+}
+
+const addRowButton = document.getElementById("addRow")
+addRowButton.innerHTML = "Add Row"
+addRowButton.onclick = () => {
+    addRow()
+}
