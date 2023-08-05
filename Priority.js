7cacf5fcf588e074eb1fac4ad25a4f3624a43af4
Jamie Bursey <jamie4551@gmail.com>
2023-08-05 11:07:49 -0230

    fixed rendering isssue

--- Priority.js
+++ Priority.js
@@ -1,7 +1,6 @@
-const tableHeader = ["Name", "Therapist", "Treatment", "Date", "Price", "Number", "Claimed"];
+const tableHeader = ["Name", "Therapist", "Treatment", "Date", "Value", "Number", "Claimed", "Delete"];
 const tableElement = document.getElementById("tableElement");
 let inputDataArray = [];
-let currentRowID = 0;
 
 const headerRow = document.createElement("tr");
 tableElement.appendChild(headerRow);
@@ -14,47 +13,64 @@ tableHeader.forEach((item) => {
 });
 
 const addRow = (values) => {
-    const tableRow = document.createElement("tr");
-    const rowID = currentRowID++;
-    tableRow.setAttribute("data-row-id", rowID);
-
-    const rowValues = values.length === tableHeader.length ? values : Array(tableHeader.length).fill("");
-    inputDataArray.push(rowValues);
-
+    const tableRowObject = {};
     tableHeader.forEach((item, index) => {
-        const tableData = document.createElement("td");
-        tableData.classList.add("table-data");
-        const dataInput = document.createElement("input");
-        dataInput.type = "text";
-        dataInput.classList.add("data-input");
-        if (index === tableHeader.length - 4) {
-            dataInput.type = "date"
-        }
-        tableData.appendChild(dataInput);
-        tableRow.appendChild(tableData);
-        if (index === tableHeader.length - 1) {
-            dataInput.style.display = "none"
-            const deleteButton = document.createElement("button");
-            deleteButton.innerHTML = "Delete";
-            deleteButton.addEventListener("click", () => {
-                if (confirm("Are you sure you want to delete this row?")) {
-                    const rowIndex = tableRow.rowIndex - 1;
-                    inputDataArray.splice(rowIndex, 1);
-                    tableElement.removeChild(tableRow);
-                    updateLocalStorage();
-                }
-            });
-            tableData.appendChild(deleteButton);
-        } else {
-            dataInput.value = rowValues[index];
-            dataInput.addEventListener("input", () => {
-                updateLocalStorage(rowID, getRowInputValues(tableRow));
-            });
-        }
+        tableRowObject[item] = values[index] || "";
     });
-    tableElement.appendChild(tableRow);
-
+    inputDataArray.push(tableRowObject);
+    renderRows();
     updateLocalStorage();
+};
+
+const deleteRow = (index) => {
+    inputDataArray.splice(index, 1);
+    renderRows();
+    updateLocalStorage();
+};
+
+const renderRows = () => {
+    while (tableElement.firstChild) {
+        tableElement.firstChild.remove();
+    }
+    tableElement.appendChild(headerRow);
+
+    inputDataArray.forEach((rowData, index) => {
+        const tableRow = document.createElement("tr");
+        tableRow.setAttribute("data-row-id", index);
+
+        tableHeader.forEach((item, columnIndex) => {
+            const tableData = document.createElement("td");
+            tableData.classList.add("table-data");
+
+            if (item === "Delete") {
+                const deleteButton = document.createElement("button"); // Create the delete button
+                deleteButton.innerHTML = "Delete";
+                deleteButton.addEventListener("click", () => {
+                    if (confirm("Are you sure you want to delete this row?")) {
+                        deleteRow(index);
+                    }
+                });
+                tableData.appendChild(deleteButton);
+            } else {
+                const dataInput = document.createElement("input");
+                dataInput.type = "text";
+                dataInput.classList.add("data-input");
+                if (item === "Date") {
+                    dataInput.type = "date";
+                }
+                tableData.appendChild(dataInput);
+                dataInput.value = rowData[item];
+                dataInput.addEventListener("input", () => {
+                    rowData[item] = dataInput.value;
+                    updateLocalStorage();
+                });
+            }
+
+            tableRow.appendChild(tableData);
+        });
+
+        tableElement.appendChild(tableRow);
+    });
 };
 
 
@@ -71,62 +87,35 @@ const loginForm = document.getElementById("login-form");
 const inputUserName = document.createElement("input");
 inputUserName.type = "text";
 inputUserName.placeholder = "User Name";
-const inputPassword = document.createElement("input");
-inputPassword.type = "password";
-inputPassword.placeholder = "Password";
 const loginSubmit = document.createElement("button");
 loginSubmit.innerHTML = "Submit";
 loginForm.appendChild(inputUserName);
-loginForm.appendChild(inputPassword);
 loginForm.appendChild(loginSubmit);
 
 loginSubmit.onclick = (event) => {
     event.preventDefault();
 
-    if (inputUserName.value === "test" && inputPassword.value === "test") {
+    if (inputUserName.value === "test") {
         tableElement.style.pointerEvents = "auto";
         addRowButton.disabled = false;
 
         const storedData = localStorage.getItem("datainput");
         inputDataArray = storedData ? JSON.parse(storedData) : [];
 
-        while (tableElement.firstChild) {
-            tableElement.firstChild.remove();
-        }
-
-        tableElement.appendChild(headerRow);
-
-        inputDataArray.forEach((item) => {
-            addRow(item);
-        });
+        renderRows();
     }
 };
 
-const updateLocalStorage = (index, value) => {
-    inputDataArray[index] = value;
-    localStorage.setItem("datainput", JSON.stringify(inputDataArray.filter(Boolean)));
-};
-
-const getRowInputValues = (row) => {
-    const inputElements = row.getElementsByClassName("data-input");
-    const values = [];
-    for (let i = 0; i < inputElements.length; i++) {
-        values.push(inputElements[i].value);
-    }
-    return values;
+const updateLocalStorage = () => {
+    localStorage.setItem("datainput", JSON.stringify(inputDataArray));
 };
 
 document.addEventListener("DOMContentLoaded", () => {
     if (localStorage.getItem("datainput") && localStorage.getItem("loggedIn") === "true") {
         inputUserName.value = "test";
-        inputPassword.value = "test";
         loginSubmit.click();
-    } else {
-
-        while (tableElement.firstChild) {
-            tableElement.firstChild.remove();
-        }
     }
 });
+
 
 
